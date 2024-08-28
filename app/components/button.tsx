010440060305d21ba3443d69f45cf7d89e9b772d
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-08-28 15:02:20 -0300

    fix: fix Button component styling issue

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps extends ComponentProps<"button"> {}
 export const Button = ({ children, className, ...rest }: ButtonProps) => {
   return (
     <button
-      className={`"bg-emerald-500 h-full aspect-square items-center justify-center flex rounded-md" ${className}`}
+      className={`bg-emerald-500 h-full aspect-square items-center justify-center flex rounded-md ${className}`}
       {...rest}>
       {children}
     </button>
